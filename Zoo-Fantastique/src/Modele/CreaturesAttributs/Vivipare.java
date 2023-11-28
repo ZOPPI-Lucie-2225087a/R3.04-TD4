41758a130dcad3a25114c792334b9e3f9146ae5e
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-28 15:34:07 +0100

    Update Vivipare.java

--- Zoo-Fantastique/src/Modele/CreaturesAttributs/Vivipare.java
+++ Zoo-Fantastique/src/Modele/CreaturesAttributs/Vivipare.java
@@ -1,10 +1,21 @@
 package CreaturesAttributs;
 
 public abstract class Vivipare extends Creature {
+
+    public abstract Vivipare creerNouveau(char sexe, double poids, double taille, int age, int indicateurFaim,
+            boolean dort, int indicateurSante);
+
     public Vivipare(String nomEspece, char sexe, double poids, double taille, int age, int indicateurFaim, boolean dort,
             int indicateurSante) {
         super(nomEspece, sexe, poids, taille, age, indicateurFaim, dort, indicateurSante);
     }
 
-    public abstract Creature mettreBas();
+    public Creature mettreBas() {
+        if (this.getSexe() == 'F') {
+            return creerNouveau('M', this.getPoids(), this.getTaille(), 0, 100, false, 100);
+        } else {
+            System.out.println("Ne peut pas mettre bas car c'est un mâle.");
+            return null;
+        }
+    }
 }
