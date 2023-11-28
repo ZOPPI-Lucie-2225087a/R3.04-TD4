9d370c186c736d5d37102f87c98392e52a7f9c34
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-28 11:04:06 +0100

    Update Aquarium.java

--- Zoo-Fantastique/src/Modele/base/Aquarium.java
+++ Zoo-Fantastique/src/Modele/base/Aquarium.java
@@ -1,12 +1,51 @@
 package base;
-import base.Enclos;
+
+import java.util.List;
+import Interface.CreatureMarine;
 
 public class Aquarium extends Enclos {
-    private int profondeur;
+    private double profondeur;
     private double salinite;
 
-    public Aquarium(int profondeur, double salinite) {
+    public Aquarium(String nom, double superficie, int capaciteMax, List<Creature> creatures, String proprete, double profondeur, double salinite) {
+        super(nom, superficie, capaciteMax, creatures, proprete);
         this.profondeur = profondeur;
+        this.salinite = salinite;
+    }
+
+    @Override
+    public void entretenir() {
+        super.entretenir();
+        verifierQualiteEau(); 
+        System.out.println("L'aquarium a éter entretenu, avec vérification de la qualité de l'eau.");
+    }
+
+    private void verifierQualiteEau() { 
+        System.out.println("La qualité de l'eau a éter vérifié : profondeur et salinité sont bonnes.");
+    }
+
+    @Override
+    public void ajouterCreature(Creature creature) {
+        if (!(creature instanceof CreatureMarine)) {
+            System.out.println("Seules les créatures aquatiques peuvent être ajoutées à un aquarium.");
+            return;
+        }
+        super.ajouterCreature(creature);
+    }
+
+    public double getProfondeur() {
+        return profondeur;
+    }
+
+    public void setProfondeur(double profondeur) {
+        this.profondeur = profondeur;
+    }
+
+    public double getSalinite() {
+        return salinite;
+    }
+
+    public void setSalinite(double salinite) {
         this.salinite = salinite;
     }
 }
