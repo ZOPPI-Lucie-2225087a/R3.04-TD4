b4afd3c7dedbf6129e39226bd1a62bbab70b73bf
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-12-01 11:05:56 +0100

    Update SimulerEnclos.java

---
+++ Zoo-Fantastique/src/Modele/Base/SimulerEnclos.java
@@ -0,0 +1,15 @@
+package Base;
+
+public class SimulerEnclos implements Runnable {
+    
+    private Enclos enclos;
+
+    public SimulerEnclos(Enclos enclos) {
+        this.enclos = enclos;
+    }
+
+    @Override
+    public void run() {
+       // a faire
+    }
+}
