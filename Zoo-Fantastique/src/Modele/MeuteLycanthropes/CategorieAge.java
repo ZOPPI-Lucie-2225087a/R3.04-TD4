a873d5806994c601b42a049d1c633526c7d0fa32
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-12-03 14:46:49 +0100

    Update CategorieAge.java

--- Zoo-Fantastique/src/Modele/MeuteLycanthropes/CategorieAge.java
+++ Zoo-Fantastique/src/Modele/MeuteLycanthropes/CategorieAge.java
@@ -3,5 +3,15 @@ package MeuteLycanthropes;
 public enum CategorieAge {
     JEUNE,
     ADULTE,
-    VIEUX
+    VIEUX;
+
+    public static CategorieAge getCategorie(int age) {
+        if (age >= 0 && age <= 30) {
+            return JEUNE;
+        } else if (age >= 31 && age <= 60) {
+            return ADULTE;
+        } else {
+            return VIEUX;
+        }
+    }
 }
