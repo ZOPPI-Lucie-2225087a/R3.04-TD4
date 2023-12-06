753831718719501231ce7c44c197a1a9d286ff41
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-12-06 22:56:05 +0100

    Create Couple_α.java

---
+++ Zoo-Fantastique/src/Modele/MeuteLycanthropes/Couple_α.java
@@ -0,0 +1,12 @@
+package MeuteLycanthropes;
+
+public class Couple_α {
+    private Lycanthrope maleAlpha;
+    private Lycanthrope femaleAlpha;
+
+    public CoupleAlpha(Lycanthrope maleAlpha, Lycanthrope femaleAlpha) {
+        this.maleAlpha = maleAlpha;
+        this.femaleAlpha = femaleAlpha;
+    }
+//a finir 
+}
