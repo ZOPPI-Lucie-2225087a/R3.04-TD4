7183e327a94ea7ebd7cd5f8e3d8a018913476a78
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-12-06 22:56:07 +0100

    Update Hurlement.java

--- Zoo-Fantastique/src/Modele/MeuteLycanthropes/Hurlement.java
+++ Zoo-Fantastique/src/Modele/MeuteLycanthropes/Hurlement.java
@@ -1,20 +1,24 @@
 package MeuteLycanthropes;
 
-public enum Hurlement { //rajouter des truc + logqiue
-    COMMUNICATION,
-    DOMINATION,
-    APPEL;
+public enum Hurlement {
+    COMMUNICATION,//- exprimer l’appartenance à une meute (entre les membres de la meute ou pour avertir les individus d’une autre meute) : lorsqu’un lycanthrope d’une meute utilise ce type de hurlements, tous les autres membres du clan qui l’entendent lui répondent en lançant un hurlement du même type (mais cette réponse n’appelle pas d’autres réponses) ; ceux des autres meutes peuvent répondre par leur propre hurlement de meute 
+    DOMINATION, //− exprimer la domination ;
+    SOUMISSION, // − exprimer la soumission (en réponse à une expression de domination) ;
+    AGRESSION; // − exprimer l’agressivité (en réponse à une expression de domination ou envers un membreω).
 
     public void hurler(Hurlement hurlement) {
         switch (hurlement) {
             case COMMUNICATION:
-                System.out.println("communication");
+                System.out.println("Je communique mon appartenance.");
                 break;
             case DOMINATION:
-                System.out.println("je domine");
+                System.out.println("Je domine.");
                 break;
-            case APPEL:
-                System.out.println("j'appelle");
+            case SOUMISSION:
+                System.out.println("Je me soumets.");
+                break;
+            case AGRESSION:
+                System.out.println("Je suis agressif.");
                 break;
         }
     }
