937f8922a31ebf3ab1ed3381a0e01038299d64ed
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-12-08 17:50:30 +0100

    Accouplement Couple Alpha

--- Zoo-Fantastique/src/Modele/MeuteLycanthropes/Couple_α.java
+++ Zoo-Fantastique/src/Modele/MeuteLycanthropes/Couple_α.java
@@ -1,8 +1,15 @@
 package MeuteLycanthropes;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Couple_α {
+
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private Lycanthrope[] coupleAlpha;
 
     public Lycanthrope trouverMaleAlpha(List<Lycanthrope> meute) {
         Lycanthrope maleAlpha = null;
@@ -55,4 +62,40 @@ public class Couple_α {
         return coupleAlpha;
     }
 
+    public void startVerifyingCoupleAlpha(List<Lycanthrope> meute) {
+        Runnable verifier = new Runnable() {
+            public void run() {
+                coupleAlpha = verifierCoupleAlpha(meute, coupleAlpha);
+            }
+        };
+
+        scheduler.scheduleAtFixedRate(verifier, 0, 5, TimeUnit.SECONDS);
+    }
+
+    public void stopVerifyingCoupleAlpha() {
+        scheduler.shutdown();
+    }
+
+    public Lycanthrope[] reproduire(boolean saisonDesAmours, List<Lycanthrope> meute) {
+        if (!saisonDesAmours) {
+            return new Lycanthrope[0];
+        }
+    
+        Random rand = new Random();
+        int nombreDeNouveauxLycanthropes = rand.nextInt(7) + 1;
+    
+        Lycanthrope[] nouveauxLycanthropes = new Lycanthrope[nombreDeNouveauxLycanthropes];
+        for (int i = 0; i < nombreDeNouveauxLycanthropes; i++) {
+            String nom = "NouveauLycan" + i;
+            char sexe = rand.nextBoolean() ? 'M' : 'F';
+            String categorieAge = CategorieAge.JEUNE.toString();
+            int rangIndex = rand.nextInt(RangDomination.values().length);
+            RangDomination rangDomination = RangDomination.values()[rangIndex];
+    
+            nouveauxLycanthropes[i] = new Lycanthrope(nom, sexe, categorieAge, rangDomination);
+            meute.add(nouveauxLycanthropes[i]);
+        }
+    
+        return nouveauxLycanthropes;
+    }
 }
